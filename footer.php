1f840218983ebcd147ba89b1b6fdfb71f30de615
andrei116 <andrei@wejyc.com>
2022-05-06 21:09:03 +0200

    Updates

--- footer.php
+++ footer.php
@@ -4,33 +4,33 @@
 
         <div class="box">
             <h3>links rápidos</h3>
-            <a href="index.php">inicio</a>
-            <a href="#">catálogo</a>
-            <a href="#">sobre nosotros</a>
-            <a href="#">contacto</a>
+            <a href="index.php">Inicio</a>
+            <a href="#">Catálogo</a>
+            <a href="#">Sobre nosotros</a>
+            <a href="#">Contacto</a>
         </div>
 
         <div class="box">
             <h3>legal</h3>
-            <a href="#">aviso legal</a>
-            <a href="#">politica de privacidad</a>
-            <a href="#">politica de cookies</a>
-            <a href="#">términos y condiciones</a>
+            <a href="#">Aviso legal</a>
+            <a href="#">Política de privacidad</a>
+            <a href="#">Política de cookies</a>
+            <a href="#">Términos y condiciones</a>
         </div>
 
-        <div class="box">
+        <div class="box" id="info">
             <h3>info contacto</h3>
-            <p> <i class="fas fa-phone"></i> 900 000 000 </p>
-            <p> <i class="fas fa-envelope"></i> contacto@gmail.com </p>
-            <p> <i class="fas fa-map-marker-alt"></i> Madrid - XYZ </p>
+            <p> <i class="fas fa-phone"></i><a href="tel:+34658543300">+34 658543300</a></p>
+            <p> <i class="fas fa-envelope"></i><a href="mailto:Andreiggar@gmail.com">Andreiggar@gmail.com</a></p>
+            <p> <i class="fas fa-map-marker-alt"></i><a href="https://www.google.com/maps/place/IES+P%C3%ADo+Baroja/@40.3729056,-3.7192079,15z/data=!4m2!3m1!1s0x0:0xb86372c70d662304?sa=X&ved=2ahUKEwiPs5qX-8f3AhUEQBoKHdlDBe8Q_BJ6BAhcEAU" target="_blank" rel="noopener">IES Pío Baroja, Madrid</a></p>
         </div>
 
         <div class="box">
             <h3>redes sociales</h3>
-            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
-            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
-            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
         </div>
 
     </div>
--- index.php
+++ index.php
@@ -70,7 +70,7 @@ if (isset($_POST['add_to_wishlist'])) {
                 while ($fetch_products = mysqli_fetch_assoc($select_products)) {
             ?>
                     <form action="" method="POST" class="box">
-                        <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+                        <a href="product_details.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
                         <div class="price"><?php echo $fetch_products['price']; ?>€</div>
                         <img src="<?php echo $fetch_products['image']; ?>" alt="" class="image">
                         <div class="name"><?php echo $fetch_products['description']; ?></div>
